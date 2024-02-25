29f6681b5fbb7bc11f96e1b3ee0a70fd2f1c99dc
Louis Donaldson <louis.donaldson42@gmail.com>
2024-02-25 23:41:17 +0000

    added links

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
+const links = [
+  {
+    name: "Pathfinding Demonstration",
+    href: "./pathfinding/pathfinding.html",
+  },
+  {
+    name: "Bywater Concept Website",
+    href: "bywater.html",
+  },
+];
+
 document.addEventListener("DOMContentLoaded", () => {
   init();
 });
@@ -13,13 +24,19 @@ async function init() {
   text.addEventListener("click", () => {
     const main_body = document.querySelector(".main_body");
     main_body.innerHTML = `
-    <ul class="ul_nav">
-        <li class="li_nav_item">
-            <a href="bywater.html">Bywater Concept Website</a>
-        </li>
-        <li class="li_nav_item">
-            <a href="./pathfinding/pathfinding.html">Pathfinding Demonstration</a>
-        </li>    </ul>`;
+    <ul class="ul_nav">    
+    </ul>`;
+
+    const ul = main_body.querySelector(".ul_nav");
+
+    for (const { name, href } of links) {
+      const li = document.createElement("li");
+      li.classList.add("li_nav_item");
+      li.innerHTML = `
+      <a href="${href}">${name}</a>`;
+
+      ul.append(li);
+    }
   });
 }
 
